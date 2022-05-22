f1876b1981ce75f426c6e08366a852b2c4c2beba
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2022-05-22 19:01:16 +0100

    Week 5 task 2 part 2.

--- db/upgrade.php
+++ db/upgrade.php
@@ -67,5 +67,20 @@ function xmldb_collaborate_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2022050104, 'collaborate');
     }
 
+    if ($oldversion < 2022050105) {
+
+        // Define field grade to be added to collaborate_submissions.
+        $table = new xmldb_table('collaborate_submissions');
+        $field = new xmldb_field('grade', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'timemodified');
+
+        // Conditionally launch add field grade.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Collaborate savepoint reached.
+        upgrade_mod_savepoint(true, 2022050105, 'collaborate');
+    }
+
     return true;
 }
